4dfe4fef0b8399a8ca16df5dc7e50a0a18ae2e78
Tyler Olsen <ty_guy5@me.com>
2015-04-16 19:12:42 -0600

    Added viewController

---
+++ InYourFace/IFViewController.h
@@ -0,0 +1,13 @@
+//
+//  IFViewController.h
+//  InYourFace
+//
+//  Created by TYLER on 4/16/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IFViewController : UIViewController
+
+@end
